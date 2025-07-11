1a061b141dd4bed8f86e5bff5b4a5762138da745
Washington Medeiros <wmedeirosm001@gmail.com>
2025-07-11 11:24:22 -0300

    feat: building page

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,24 +1,35 @@
-import { styled } from "@mui/material";
+import { Container, Grid, styled, Typography } from "@mui/material";
 import Avatar from "../../../../assets/images/avatar.jpeg";
 
 const StyledHero = styled("div")(() => ({
-  backgroundColor: "#000", 
-  
+  backgroundColor: "#000",
+  padding: "40px 0", // adicionei um padding básico para evitar que o conteúdo grude nas bordas
 }));
 
 const StyledImg = styled("img")(() => ({
-  backgroundColor: "#f0f0f0",
-  padding: "20px",
+  width: "30%",
   borderRadius: "50%",
-  width: "39%"
 }));
 
 const App = () => {
   return (
     <>
       <StyledHero>
-        Olá
-        <StyledImg src={Avatar} alt="Avatar" />
+        <Container>
+          <Grid container spacing={2} alignItems="center">
+            <Grid size={4}>
+              <StyledImg src={Avatar} alt="Avatar" />
+            </Grid>
+            <Grid size={8}>
+              <Typography color="primary" variant="h1">
+                Washington Medeiros
+              </Typography>
+              <Typography color="primary" variant="h2">
+                I'm a Computer Scientist
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
       </StyledHero>
     </>
   );
